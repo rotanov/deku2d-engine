090bd9a1960f88964541b44e12a67598f28dc911
Alexander Nartov <alexander.nartov@gmail.com>
2010-04-10 17:22:16 +0000

    Добавлен Makefile для "Demo template". Фикс совместимости с виндовым dirent.h (перенесён из new-factory). Добавлена директория Logs в Demos/Demo template/bin/.

--- Engine/src/2de_Resource.cpp
+++ Engine/src/2de_Resource.cpp
@@ -231,6 +231,7 @@ void CDataLister::ExploreDirectory(string Path)
 			CurNode = CurNode->Add(entry->d_name, entry->d_name);
 			ExploreDirectory(Path + "/" + string(entry->d_name));
 			CurNode = PreviousNode;
+			closedir(testdir);
 		}
 		//else if (entry->d_type == DT_REG)
 		else
@@ -239,7 +240,6 @@ void CDataLister::ExploreDirectory(string Path)
 				     Path + "/" + string(entry->d_name));
 		}
 
-		closedir(testdir);
 	}
 	closedir(dirp);
 }
